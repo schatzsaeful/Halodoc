264ab554df6d9009504205093d26405e019df726
Saeful Husna <schatzsaeful@gmail.com>
2022-10-06 21:14:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/halodoc/module/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.halodoc.module.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.halodoc.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/halodoc/module/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.halodoc.module.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.halodoc.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
